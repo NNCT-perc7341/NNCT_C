e41534433df85c61f0420f5c9ff4faf6f488f93f
K.Hoshino <72436798+NNCT-perc7341@users.noreply.github.com>
2022-02-10 23:03:33 +0900

    Update assignment_2_show_kohei.c

--- exercise/assignment_2_show_kohei.c
+++ exercise/assignment_2_show_kohei.c
@@ -5,14 +5,14 @@
 #define N2 5
 
 int main(void);
-int a = 6
+int a = 6;
 int ab = 5;
 
 int main(void){
   int x[N1][N2] = {{3,2,4,5,6},{4,2,3,1,0},{5,0,6,3,3},{4,0,5,0,0},{2,1,3,3,0},{1,3,0,5,4}};
   int y[N2] = {145,121,72,34,25};
-  int z[N1] = {0}
-  int = zz[N1] = {0};
+  int z[N1] = {0};
+  int zz[N1] = {0};
 
   char *c[] = {"安藤","笠井","佐野","小林","杉本","田中"} ;
 
@@ -36,32 +36,32 @@ int main(void){
 
   printf("安藤¥t");
   for (int m = 0; m < ab; m++) {
-    printf("%3d個¥t", x[0][m])
+    printf("%3d個¥t", x[0][m]);
   }
   printf("¥n");
   printf("笠井¥t");
   for (int m = 0; m < ab; m++) {
-    printf("%3d個¥t", x[1][m])
+    printf("%3d個¥t", x[1][m]);
   }
   printf("¥n");
   printf("佐野¥t");
   for (int m = 0; m < ab; m++) {
-    printf("%3d個¥t", x[2][m])
+    printf("%3d個¥t", x[2][m]);
   }
   printf("¥n");
   printf("小林¥t");
   for (int m = 0; m < ab; m++) {
-    printf("%3d個¥t", x[3][m])
+    printf("%3d個¥t", x[3][m]);
   }
   printf("¥n");
   printf("杉本¥t");
   for (int m = 0; m < ab; m++) {
-    printf("%3d個¥t", x[4][m])
+    printf("%3d個¥t", x[4][m]);
   }
   printf("¥n");
   printf("田中¥t");
   for (int m = 0; m < ab; m++) {
-    printf("%3d個¥t", x[5][m])
+    printf("%3d個¥t", x[5][m]);
   }
 
   printf("¥n");
